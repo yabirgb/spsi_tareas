36093bcfe0ec9fc574bf561109bd64ad525b74b0
Yabir Garcia <yabirg@protonmail.com>
2020-11-28 20:04:52 +0100

    Ejercicios 1 y 2 comentados

--- ejercicio1.py
+++ ejercicio1.py
@@ -1,42 +1,83 @@
 from fractions import Fraction
 
-# Apartado a
+"""
+Ejercicio 1. 
+
+En este fichero se tienen las distintas modificaciones del algoritmo descrito y
+una versión final que incluye a todas y que calcula a^b con la posibilidad de
+hacer a^b mod n.
+"""
+
+# En primer lugar presentamos la versión de la función tal y como se describe
 
 def exponenciacion(a,b):
 
+    """
+    Calcula a^b de forma recursiva
+    """
+
+    # Introducimos el caso base
     if b == 0:
         return 1
     
+    # Aplicamos al recursión según la paridad de b
     if b > 0 and (b % 2 == 0):
+        # Aquí utilizamos el corrimiento de bits para dividir entre 2
         return exponenciacion(a**2, b >> 1)
     elif b>0 and (b % 2 == 1):
         return a*exponenciacion(a**2, (b-1)>>1)
 
+
+
 def exponenciacion_iterativa(s,t):
 
+    """
+    Calculamos a^b implementando el algoritmo descrito pero esta vez
+    de manera iterativa. Esto lo hacemos porque python funciona peor
+    al trabajar con funciones recursivas.
+    """
+
+    # Introducimos una variable acumulador que 
+    # almacena el resultado que vamos a devolver
     acumulador = 1
 
+    # creamos una copia de las variables.
     a,b = s,t
 
+    # Traducimos la confición de parada del caso recursivo
     while b != 0:
         if b%2 == 0:
+            # En lugar de hacer la llamada recursiva modificamos las variables
+            # a y b
             a,b = a**2, b>>1
         else:
+            # En nuestra función recursiva multiplicavamos el valor de la
+            # recursión por a. En esta versión multiplicamos el acumulador por a
+            # y actualizamos los valores de la iteración
             acumulador *= a 
             a,b = a**2, (b-1)>>1
-
+    # Devolvemos a^b
     return acumulador
 
 
-#print(exponenciacion(256,120)==exponenciacion_iterativa(256,120))
 
-# apartado b
-
-# a^(-b) == (a^b)^(-1)
 def exp_inverse(a,b):
+
+    """
+    Calculamos el valor de a^(-b). Nos damos cuenta de que este valor coincide
+    con (a^b)^(-1) y por tanto podemos utilizar la función que ya habíamos
+    definido anteriormente. Utilizamos el modulo fractions de python para
+    representar el número que se obtiene.
+    """
+
     return Fraction(1,exponenciacion_iterativa(a,b))
 
-# apartado c
+
+"""
+Definimos ahora el algoritmo de euclides extendido que utilizamos para calcular
+los coeficientes de Bezout. Esto nos hará falta cuando queramos calcular la potencia
+(a^(-b)) mod n
+"""
 
 def bezout(a, b):
     # Hacemos que a sea el mayor de los dos numeros
@@ -59,44 +100,68 @@ def bezout(a, b):
     else:
         return r1,s1,t1
 
-# print(bezout(23, 48))
-
 def exp_mod_n(a,b,n):
+
+    """
+    Función para calcular el valor de [(a^b) mod n] con n >= 0 entero.
+    """
+
+    # Nuestro objetivo va aser calcular (a^abs(b)) y si b es negativo tomaremos
+    # la precaución de calcular el inverso de (a^b) mod n
+    
+    # Si b es negativo lo hacemos positivo y advertimos de que b era negativo
     f_inverse=False
     if b<0:
         b=-b
         f_inverse = True
 
+    # Calculamos a^b. Aqui tenemos la precaucion de hacer a%n antes de operar.
     exp = exponenciacion_iterativa(a%n,b)
+
+    # Si tenemos que calcular el inverso
     if f_inverse:
+        # Calculamos los coeficientes de bezout siendo el primero que devolvemos
+        # el inverso de exp
         mcd, inverse, t1 = bezout(exp%n, n)
         
         # Si el maximo comun divisor es 1, hay solución
         if mcd == 1:
+            # Si el inverso es negativo mod n lo potemos positivo
             if inverse<0:
                 return inverse+n
             return inverse
         else:
+            # En otro caso levantamos una excepcion indicando que no existe solucion
             raise Exception("Inverse doesn't exists")
     
     return exp%n
 
 
 def exp(a,b,n=None):
+
+    """
+    Función para calcular [a^b mod n] o [a^b]
+    """
+
+    # Si n no es None es que queremos calcular exp mod n
     if n:
+        # usamos la funcion definida
         return exp_mod_n(a,b,n)
     if b<0:
+        # Si b es negativo calculamos el inverso de a^b
         return exp_inverse(a,-b)
+    # En otro caso podemos calcular a^b de manera directa
     return exponenciacion_iterativa(a,b)
         
     
 
 
+# Algunas pruebas. Si todo funciona bien al ejecutar este script no debería producirse ningun fallo
 
-#print(exp_mod_n(256,120, 8597)+8597)
-assert(exp_mod_n(256,120,8597)==3696)
-assert(exp_mod_n(256,-120,8597)==7748)
-assert(exp(256,120,8597)==3696)
-assert(exp(256,-120,8597)==7748)
-assert(exp(256,120)==256**120)
-assert(exp(256,-120)==Fraction(1,256**120))
+if __name__ == '__main__':
+    assert(exp_mod_n(256,120,8597)==3696)
+    assert(exp_mod_n(256,-120,8597)==7748)
+    assert(exp(256,120,8597)==3696)
+    assert(exp(256,-120,8597)==7748)
+    assert(exp(256,120)==256**120)
+    assert(exp(256,-120)==Fraction(1,256**120))
--- ejercicio2.py
+++ ejercicio2.py
@@ -1,6 +1,21 @@
 from string import ascii_lowercase
 
 class Vigenere:
+    """
+    Clase para manejar las funcionalidades basicas del cifrado con Vigenere.
+
+    Para inicializar la clase se necesita proporcionar:
+
+    - Un alfabeto en formato string
+    - Una clave en formato string
+
+    Se proporcionan los metodos
+
+    - encode(msg)
+    - cipher(msg)
+    - decipher(msg)
+
+    """
 
     def __init__(self, alphabet, key):
 
@@ -8,15 +23,27 @@ class Vigenere:
         self.key = key
 
     def encode(self, msg):
+        """
+        Codificamos el menaje de entrada eliminando los caracteres que no
+        pertenecen al alfabeto.
+        """
         
         return "".join([x for x in msg if x in self.alphabet])
 
     def _set_key_length(self, target_length):
+        """
+        Función auxiliar para repetir la clave tantas veces como sea neceario
+        hasta completar una cadena que sea tan larga como el mensaje que se
+        quiere codificar.
+        """
 
         n, r = divmod(target_length,len(self.key))
         return self.key*n + self.key[:r]
 
     def _add_chars(self, a,b):
+        """
+        Operación para sumar caracteres dentro del alfabeto proporcionado
+        """
 
         ai = self.alphabet.index(a)
         bi = self.alphabet.index(b)
@@ -24,26 +51,59 @@ class Vigenere:
         return self.alphabet[(ai + bi) % len(self.alphabet)]
 
     def _diff_chars(self, a,b):
-    
+        """
+        Operación para restar caracteres dentro del alfabeto proporcionado
+        """
+
         ai = self.alphabet.index(a)
         bi = self.alphabet.index(b)
 
         return self.alphabet[(ai - bi) % len(self.alphabet)]
 
     def cipher(self, msg):
+        """
+        Metodo para codificar un mensaje
+        """
+
+        # En primer lugar codificamos el mensaje
         msg_encoded = self.encode(msg)
+        # Repetimos la clave hasta que tiene la longitud del mensaje codificado
         key = self._set_key_length(len(msg_encoded))
-        #print(msg_encoded)
-        #print(len(key),len(msg_encoded))
+        # Sumamos caracter a carater mensaje codificado y clave
         return "".join(map(lambda x: self._add_chars(x[0],x[1]), zip(key, msg_encoded)))
 
     def decipher(self, msg):
+        """
+        Metodo para decodifar un mensaje
+        """
+        
+        # En primer lugar elimanamos caracteres que no pertenezcan al alfabeto elegido
         msg_encoded = self.encode(msg)
+        # Repetimos la clave hasta que alcanza una longitud deseada
         key = self._set_key_length(len(msg_encoded))
-        #print(msg_encoded)
-        #print(len(key),len(msg_encoded))
+        # Aplicamos la operación de diferencia entre el mensaje cifrado y la clave
         return "".join(map(lambda x: self._diff_chars(x[0],x[1]), zip(msg_encoded, key)))
 
-#V = Vigenere(ascii_lowercase, "lauraguapa")
-#ciph=V.cipher("era una noche de verano cuando el asesino y la victima se cruzaron en lo que se conocia como el jardin de los tristes. era un momento en el que ambos supieron quie se acercaba una desgracia pero ninguno podía hacer nada para evitar el desastre, tres segundos más tarde solo quedaba un alma en la tierra y una nueva andaba por los jardines del paraiso.")
-#print(V.decipher(ciph))
+
+if __name__ == '__main__':
+
+    # Ejemplo de uso
+
+    # Inicializamos la clase con el alphabeto de los caracteres en minuscula y
+    # la clabe lauranover
+
+    V = Vigenere(ascii_lowercase, "lauranover")
+    #text = "era una noche de verano cuando el asesino y la victima se cruzaron en lo que se conocia como el jardin de los tristes. era un momento en el que ambos supieron quie se acercaba una desgracia pero ninguno podía hacer nada para evitar el desastre, tres segundos más tarde solo quedaba un alma en la tierra y una nueva andaba por los jardines del paraiso."
+    
+    # Texto que vamos a cifrar
+    text = """the inmesurable days were comming to an end while the people in the village were thinking about a possible solution to the big problem. Many were sad because the crops didnt provide enought food for the children, other were sad because they could scape from
+    the reallity that a inmense monster called the death was walking in the streets waiting for someone to exist their houses to 
+    end their suffering."""
+
+    # Lo ciframos
+    ciph=V.cipher(text)
+    # Imprimimos el texto cifrado
+    print(ciph)
+    #Imprimimos el texto descifrado
+    print("------")
+    print(V.decipher(ciph))
