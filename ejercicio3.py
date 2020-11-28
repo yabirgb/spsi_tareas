2b3ddd8f31f58d0ae8aaede5beb27c3d677f6ebb
Yabir Garcia <yabirgb@gmail.com>
2020-11-28 22:03:40 +0100

    Añade cometnarios y reestructura en una clase

--- ejercicio3.py
+++ ejercicio3.py
@@ -8,147 +8,209 @@ from collections import defaultdict, Counter
 from ejercicio2 import Vigenere
 import string
 
-MIN_COUNT = 3
-MAX_COUNT = 6
+class Cracker:
 
-def get_distance(substring, offset, substring_map, distances):
+    """
+    Ataque al cifrado de vigenere.
 
-    if substring in substring_map:
-        last_offset = substring_map.get(substring)
-        distances.add(offset-last_offset)
-    else:
-        substring_map[substring] = offset
+    La clase toma dos cadenas de texto como argumento:
+
+    - alph: Alfabeto que se quiere usar
+    - p: Vector de numeros con las probabilidades en el lenguaje de cada simbolo del alfabeto
+
+    Se exponen los siguientes metodos
+
+    attack(text): Intenta obtener el texto cifrado correspondiente a text
+    """
+
+    def __init__(self, alph, p, minc=3, maxc=6):
+        self.alph = alph 
+        self.p = p
+
+        self.MIN_COUNT = minc
+        self.MAX_COUNT = maxc
 
 
-def find_repeated_substrings(text:str, substring_map:Dict[str, int], distances:set):
-
-    count, offset = MIN_COUNT, 0
-
-    while (offset + count < len(text)):
-        while (offset + count < len(text) and MAX_COUNT >= count):
-            substring = text[offset:offset+count]
-            get_distance(substring, offset, substring_map, distances)
-            count +=1
-
-        offset += 1
-        count = MIN_COUNT
-
-def gcd_distance(distances):
-    dst=list(distances)
-    g=dst[0]
-    for i in dst[1:]:
-        prev = g
-        g=gcd(g,i)
-        if g == 1:
-            g=prev
-    return g
-
-def count_distance_divisors(distance:int):
-    divisors=defaultdict(int)
-    divisors[distance] =1
-    div = 2
-    while div < int(sqrt(distance)):
-
-        if distance % div == 0:
-            divisors[div] += 1
-            divisors[distance//div] += 1
-            distance = distance // div 
+    def _get_distance(self, substring, offset, substring_map, distances):
+        """
+        Encuentra la distancia entre una cadena de texto y la ultima vez que aparecio
+        """
+        if substring in substring_map:
+            last_offset = substring_map.get(substring)
+            distances.add(offset-last_offset)
         else:
-            div += 1
- 
-    return divisors
+            substring_map[substring] = offset
 
-'''def find_distances_divisors(divisors:Dict[int, int], distances:set):
+    def _find_repeated_substrings(self, text:str, substring_map:Dict[str, int], distances:set):
+        
+        """
+        Dado un texto busca las cadenas que se reptien. Busca por cadenas que se
+        repitan con una longitud minima de MIN_COUNT y una longitud maxima de
+        MAX_COUNT. 
 
-    for dist in distances:
-        count_distance_divisors(dist, divisors)
-'''
-def extract_frequencies(s:str, alph:str):
-    n_app=Counter(s)
-    freq=[]
-    for char in alph:
-        n=n_app.get(char)
-        if n:
-            freq.append(n/len(s))
-        else:
-            freq.append(0)
-    return freq   
+        Para buscar las ocurrencias realizamos un desplazamiento que empieza en
+        0 y se mueve una posicion en cada iteracion y buscamos por repeticiones
+        de las distintas posiciones posibles.
+        """
 
-def MIC(p, s:str, alph:str):
-    s_freq = extract_frequencies(s, alph)
-    return sum(map(lambda x:x[0]*x[1],zip(p,s_freq)))/len(s)
+        count, offset = self.MIN_COUNT, 0
 
-def shift_by_n(s:str, n:int, alph:str):
-    return "".join([alph[(alph.index(char)+n)%len(alph)] for char in s])
+        while (offset + count < len(text)):
+            while (offset + count < len(text) and self.MAX_COUNT >= count):
+                substring = text[offset:offset+count]
+                self._get_distance(substring, offset, substring_map, distances)
+                count +=1
 
-def compute_most_probable(s:str,p, alph:str):
-    mic=[]
-    for i in range(len(alph)):
-        mic.append(MIC(p, shift_by_n(s,i,alph), alph))
-    #print(mic)
-    candidate=max(enumerate(mic), key=lambda x: x[1])[0]
-    return (len(alph)-candidate)%len(alph)
+            offset += 1
+            count = self.MIN_COUNT
 
+    def _gcd_distance(self,distances):
+        """
+        Calcula el maximo comun divisor de una lista de numeros
+        """
+        dst=list(distances)
+        g=dst[0]
+        for i in dst[1:]:
+            prev = g
+            g=gcd(g,i)
+            if g == 1:
+                g=prev
+        return g
 
-def cracker(msg, k, alph, p):
+    def _count_distance_divisors(self, distance:int):
+        """
+        Funcion para extraer los divisores de un numero.
+        Buscamos los divisores hasta la raiz
+        """
+
+        divisors=defaultdict(int)
+        divisors[distance] =1
+        div = 2
+        while div < int(sqrt(distance)):
+
+            if distance % div == 0:
+                divisors[div] += 1
+                divisors[distance//div] += 1
+                distance = distance // div 
+            else:
+                div += 1
     
-    s_i=[[]for i in range(k)]
-    pos=0
-    for c in msg:
-        s_i[pos].append(c)
-        pos+=1
-        if pos==k:
-            pos=0
-    
-    for i,lst in enumerate(s_i):
-        s_i[i]="".join(lst)
+        return divisors
 
-    keys=[]
-    for lst in s_i:
-        keys.append(compute_most_probable(lst,p,alph))
-    #print(keys)
-    return "".join([alph[i] for i in keys]) 
-    #return keys
+    def _extract_frequencies(self, s:str):
+        """
+        Calcular la frecuencia de cada caracter del alfabeto 
+        en el texto
+        """
+        # Obtenemos el numero de veces que aparece cada caracter en el texto
+        n_app=Counter(s)
+        # Lista que vamos a devolver con las frecuencias
+        freq=[]
+        # Para cada caracter del alfabeto comprobamos si esta o no en el texto.
+        # Si esta su frecuencia es el numero de veces que aparece entre el
+        # numero de caracteres del texto. Si no esta, su frecuencia es 0
+        for char in self.alph:
+            n=n_app.get(char)
+            if n:
+                freq.append(n/len(s))
+            else:
+                freq.append(0)
+        return freq   
 
-def kasiski(intxt):
-    substrings = dict()
-    divisors = defaultdict(int)
-    distances = set()
-    find_repeated_substrings(intxt, substrings, distances)
-    distance=gcd_distance(distances)
-    #print(distance)
-    #print(count_distance_divisors(distance))
-    return count_distance_divisors(distance)
+    def MIC(self, s:str):
+        # Funcion MIC tal y como se define en el articulo
+        s_freq = self._extract_frequencies(s)
+        return sum(map(lambda x:x[0]*x[1],zip(self.p,s_freq)))/len(s)
 
-p=[0.1253, 0.014199999999999999, 0.046799999999999994, 0.058600000000000006, 0.1368, 0.0069, 0.0101, 0.006999999999999999, 0.0625, 0.0044, 0.0002, 0.049699999999999994, 0.0315, 0.06709999999999999, 0.0868, 0.025099999999999997, 0.0088, 0.0687, 0.07980000000000001, 0.0463, 0.0393, 0.009000000000000001, 0.0001, 0.0022, 0.009000000000000001, 0.0052]
+    def _shift_by_n(self, s:str, n:int):
+        # Desplaza los caracteres de una cadena de texto por una cantidad fija
+        return "".join([self.alph[(self.alph.index(char)+n)%len(self.alph)] for char in s])
 
-#intxt = """UECWKDVLOTTVACKTPVGEZQMDAMRNPDDUXLBUICAMRHOECBHSPQLVIWOFFEAILPNTESMLDRUURIFAEQTTPXADWIAWLACCRPBHSRZIVQWOFROGTTNNXEVIVIBPDTTGAHVIACLAYKGJIEQHGECMESNNOCTHSGGNVWTQHKBPRHMVUOYWLIAFIRIGDBOEBQLIGWARQHNLOISQKEPEIDVXXNETPAXNZGDXWWEYQCTIGONNGJVHSQGEATHSYGSDVVOAQCXLHSPQMDMETRTMDUXTEQQJMFAEEAAIMEZREGIMUECICBXRVQRSMENNWTXTNSRNBPZHMRVRDYNECGSPMEAVTENXKEQKCTTHSPCMQQHSQGTXMFPBGLWQZRBOEIZHQHGRTOBSGTATTZRNFOSMLEDWESIWDRNAPBFOFHEGIXLFVOGUZLNUSRCRAZGZRTTAYFEHKHMCQNTZLENPUCKBAYCICUBNRPCXIWEYCSIMFPRUTPLXSYCBGCCUYCQJMWIEKGTUBRHVATTLEKVACBXQHGPDZEANNTJZTDRNSDTFEVPDXKTMVNAIQMUQNOHKKOAQMTBKOFSUTUXPRTMXBXNPCLRCEAEOIAWGGVVUSGIOEWLIQFOZKSPVMEBLOHLXDVCYSMGOPJEFCXMRUIGDXNCCRPMLCEWTPZMOQQSAWLPHPTDAWEYJOGQSOAVERCTNQQEAVTUGKLJAXMRTGTIEAFWPTZYIPKESMEAFCGJILSBPLDABNFVRJUXNGQSWIUIGWAAMLDRNNPDXGNPTTGLUHUOBMXSPQNDKBDBTEECLECGRDPTYBVRDATQHKQJMKEFROCLXNFKNSCWANNAHXTRGKCJTTRRUEMQZEAEIPAWEYPAJBBLHUEHMVUNFRPVMEDWEKMHRREOGZBDBROGCGANIUYIBNZQVXTGORUUCUTNBOEIZHEFWNBIGOZGTGWXNRHERBHPHGSIWXNPQMJVBCNEIDVVOAGLPONAPWYPXKEFKOCMQTRTIDZBNQKCPLTTNOBXMGLNRRDNNNQKDPLTLNSUTAXMNPTXMGEZKAEIKAGQ"""
-intxt = "zpgdlrjlajkpylxzpyyglrjgdlrzhzqyjzqrepvmswrzyrigzhzvregkwivssaoltnliuwoldieaqewfiiykhbjowrhdogcqhkwajyaggemisrzqoqhoavlkbjofrylvpsrtgiuavmswlzgmsevwpcdmjsvjqbrnklpcfiowhvkxjbjpmfkrqthtkozrgqihbmqsbivdardymqmpbunivxmtzwqvgefjhucborvwpcdxuwftqmoowjipdsfluqmoeavljgqealrktiwvextvkrrgxani"
+    def compute_most_probable(self, s:str):
+        mic=[]
+        # usamos la funcion MIC para calcular cuanto se parecen las
+        # distribuciones del lenguaje a las que aparecen en la cadena de texto
+        # estudiada. Para ello rotamos la cadena s_i todas las cantidades
+        # posibles y guardamos el valor obtenido
+        for i in range(len(self.alph)):
+            mic.append(self.MIC(self._shift_by_n(s,i)))
+        
+        # Obtenemos el mayor valor obtenido para la funcion MIC. Los valores
+        # estan ordenados en funcion de su posicion asi que usamos la funcion
+        # enumerate para obtener las posiciones, elegimos el maximo por valor de
+        # la funcion y nos quedamos con su posicion que corresponde con la cantidad desplazada
+        candidate=max(enumerate(mic), key=lambda x: x[1])[0]
 
-#print(intxt)
+        # Devolvemos como se deice en el articulo el candidado a desplazamiento
+        return (len(self.alph)-candidate)%len(self.alph)
 
-"""substrings = dict()
-divisors = defaultdict(int)
-distances = set()
-find_repeated_substrings(intxt, substrings, distances)
-find_distances_divisors(divisors, distances)
-#print(divisors)
+    def kasiski(self, intxt):
+        substrings = dict()
+        divisors = defaultdict(int)
+        distances = set()
+        self._find_repeated_substrings(intxt, substrings, distances)
+        distance=self._gcd_distance(distances)
+        return self._count_distance_divisors(distance)
 
-c = Counter(divisors)
-print(c.most_common(5))"""
+    def cracker(self, msg, k):
+        # En s_i almacenamos las cadenas de caracteres que aparecen cada i
+        # posiciones en el texto de partida
+        s_i=[[]for i in range(k)]
+        pos=0
+        for c in msg:
+            s_i[pos].append(c)
+            pos+=1
+            if pos==k:
+                pos=0
+        # Los caracacteres estan almacenados como una lista y los queremos como
+        # una cadena de texto
+        for i,lst in enumerate(s_i):
+            s_i[i]="".join(lst)
 
-def break_code(intxt, alph, p):
-    keys = []
-    for length in kasiski(intxt):
-        keys.append(cracker(intxt,length,alph,p))
+        # Ahora obtenemos las claves que se consideran posibles
+        keys=[]
+        for lst in s_i:
+            keys.append(self.compute_most_probable(lst))
 
-    return keys
+        # La clave la hemos obtenido como una cadena de posiciones. Traducimos
+        # dichas posiciones a una clave de caracteres del alfabeto
+        return "".join([self.alph[i] for i in keys]) 
 
-alph = string.ascii_lowercase
-#alph = string.ascii_uppercase
+    def _break_code(self, intxt):
+        """
+        Aplicando el teste de kasiski obtenemos una lista de posibles longitudes
+        de claves y obtenemos las claves asociadas a dichas longitudes.
 
-for key in break_code(intxt, alph, p):
-    V = Vigenere(alph, cracker(intxt,len(key),alph,p))
-    print(f"Llave con longitud {len(key)}: {key}")
-    print("Texto: ")
-    print(V.decipher(intxt))
+        Finalmente devolvemos el conjunto de claves que se creen posibles.
+        """
+        
+        keys = []
+
+        for length in self.kasiski(intxt):
+            keys.append(self.cracker(intxt, length))
+
+        return keys
+
+    def attack(self, intxt):
+        # ataque al cifrado de vigenere
+
+        # Para cada clave candidata obtenida usamos la clase vigenre
+        # para descifrar usando dicha clave e imprimos el texto obtenido
+        for key in self._break_code(intxt):
+            V = Vigenere(self.alph, self.cracker(intxt,len(key)))
+            print(f"Llave con longitud {len(key)}: {key}")
+            print("Texto: ")
+            print(V.decipher(intxt))
+
+
+p = [0.1253, 0.014199999999999999, 0.046799999999999994, 0.058600000000000006, 
+        0.1368, 0.0069, 0.0101, 0.006999999999999999, 0.0625, 0.0044, 0.0002, 
+        0.049699999999999994, 0.0315, 0.06709999999999999, 0.0868, 0.025099999999999997, 
+        0.0088, 0.0687, 0.07980000000000001, 0.0463, 0.0393, 0.009000000000000001, 0.0001, 
+        0.0022, 0.009000000000000001, 0.0052]
+
+intxt = """UECWKDVLOTTVACKTPVGEZQMDAMRNPDDUXLBUICAMRHOECBHSPQLVIWOFFEAILPNTESMLDRUURIFAEQTTPXADWIAWLACCRPBHSRZIVQWOFROGTTNNXEVIVIBPDTTGAHVIACLAYKGJIEQHGECMESNNOCTHSGGNVWTQHKBPRHMVUOYWLIAFIRIGDBOEBQLIGWARQHNLOISQKEPEIDVXXNETPAXNZGDXWWEYQCTIGONNGJVHSQGEATHSYGSDVVOAQCXLHSPQMDMETRTMDUXTEQQJMFAEEAAIMEZREGIMUECICBXRVQRSMENNWTXTNSRNBPZHMRVRDYNECGSPMEAVTENXKEQKCTTHSPCMQQHSQGTXMFPBGLWQZRBOEIZHQHGRTOBSGTATTZRNFOSMLEDWESIWDRNAPBFOFHEGIXLFVOGUZLNUSRCRAZGZRTTAYFEHKHMCQNTZLENPUCKBAYCICUBNRPCXIWEYCSIMFPRUTPLXSYCBGCCUYCQJMWIEKGTUBRHVATTLEKVACBXQHGPDZEANNTJZTDRNSDTFEVPDXKTMVNAIQMUQNOHKKOAQMTBKOFSUTUXPRTMXBXNPCLRCEAEOIAWGGVVUSGIOEWLIQFOZKSPVMEBLOHLXDVCYSMGOPJEFCXMRUIGDXNCCRPMLCEWTPZMOQQSAWLPHPTDAWEYJOGQSOAVERCTNQQEAVTUGKLJAXMRTGTIEAFWPTZYIPKESMEAFCGJILSBPLDABNFVRJUXNGQSWIUIGWAAMLDRNNPDXGNPTTGLUHUOBMXSPQNDKBDBTEECLECGRDPTYBVRDATQHKQJMKEFROCLXNFKNSCWANNAHXTRGKCJTTRRUEMQZEAEIPAWEYPAJBBLHUEHMVUNFRPVMEDWEKMHRREOGZBDBROGCGANIUYIBNZQVXTGORUUCUTNBOEIZHEFWNBIGOZGTGWXNRHERBHPHGSIWXNPQMJVBCNEIDVVOAGLPONAPWYPXKEFKOCMQTRTIDZBNQKCPLTTNOBXMGLNRRDNNNQKDPLTLNSUTAXMNPTXMGEZKAEIKAGQ"""
+#intxt = "zpgdlrjlajkpylxzpyyglrjgdlrzhzqyjzqrepvmswrzyrigzhzvregkwivssaoltnliuwoldieaqewfiiykhbjowrhdogcqhkwajyaggemisrzqoqhoavlkbjofrylvpsrtgiuavmswlzgmsevwpcdmjsvjqbrnklpcfiowhvkxjbjpmfkrqthtkozrgqihbmqsbivdardymqmpbunivxmtzwqvgefjhucborvwpcdxuwftqmoowjipdsfluqmoeavljgqealrktiwvextvkrrgxani"
+
+c = Cracker(string.ascii_uppercase, p)
+c.attack(intxt)
